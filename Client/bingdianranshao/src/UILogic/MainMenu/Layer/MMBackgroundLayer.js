59bd6bf8e29eb35d41397bef7b3cca0d9722b7fc
chenxin <1017370773@qq.com>
2016-04-11 01:45:23 +0800

    [组队背包界面]

--- Client/bingdianranshao/src/UILogic/MainMenu/Layer/MMBackgroundLayer.js
+++ Client/bingdianranshao/src/UILogic/MainMenu/Layer/MMBackgroundLayer.js
@@ -13,14 +13,15 @@ var MMBackgroundLayer = cc.Layer.extend({
         g_this = this;
     },
     receiveMessage : function(){
-
         var id = GameJoy.Proxy.RecvResponse();
         if(id > 0)
         {
+            cc.log(id);
             var index = ""+id;
             var login_res = NetConfig[index]();//GameJoy.JS_CSLoginResponse.Instance();
             cc.log(login_res.get_uin());
             cc.log(login_res.get_result());
+            g_this.receiveMessageLogin();
         }
     },
     playMusic: function () {
@@ -37,7 +38,10 @@ var MMBackgroundLayer = cc.Layer.extend({
         g_mainscene = ccs.load(res.LI_LoginJson_json).node;
         this.addChild(g_mainscene);
         var l_loginButton = ccui.helper.seekWidgetByName(g_mainscene, "m_Button_login");
-        l_loginButton.addTouchEventListener(this.buttonTouchEvent);
+        var flare = new cc.Sprite(res.MM_Flare_jpg);
+        this.addChild(flare);
+        this.flareEffect(flare);
+        l_loginButton.addClickEventListener(this.buttonTouchEvent);
     },
     buttonTouchEvent: function () {
         var flare = new cc.Sprite(res.MM_Flare_jpg);
@@ -47,12 +51,12 @@ var MMBackgroundLayer = cc.Layer.extend({
         var l_text_pwd = ccui.helper.seekWidgetByName(g_mainscene, "m_TextField_pwd");
         var l_name = l_text_name.getString();
         var l_pwd = l_text_pwd.getString();
+        g_this.parent.m_Name = l_name;
         // 向服务器请求登录
         var login_request = GameJoy.JS_CSLoginRequest.Instance();
         login_request.set_username(l_name);
         login_request.set_password(l_pwd);
         GameJoy.Proxy.SendRequest(1);
-
     },
     flareEffect : function(flare){
         flare.stopAllActions();
@@ -66,32 +70,29 @@ var MMBackgroundLayer = cc.Layer.extend({
             rotation: -120,
             scale: 0.2
         });
-
-
 //        定义动作
         var opacityAnim = cc.fadeIn(0.5);
         var opacDim = cc.fadeIn(1);
-
 //        为动作加上easing效果，具体参考tests里面的示例
         var biggerEase = cc.scaleBy(0.7, 1.2, 1.2).easing(cc.easeSineOut());
         var easeMove = cc.moveBy(0.5, cc.p(640, 0)).easing(cc.easeSineOut());
         var rotateEase = cc.rotateBy(2.5, 90).easing(cc.easeExponentialOut());
         var bigger = cc.scaleTo(0.5, 1);
-
 //        函数回调动作
         var killflare = cc.callFunc(function () {
             this.getParent().removeChild(this,true);
         }, flare);
-
 //        按顺序执行一组动作
         var seqAction = cc.sequence(opacityAnim, biggerEase, opacDim, killflare);
-
 //        同时执行一组动作
         var action = cc.spawn(seqAction, easeMove, rotateEase, bigger);
         flare.runAction(action);
     },
     receiveMessageLogin: function ()
     {
-
+        this.unschedule(this.receiveMessage());
+        g_this.parent.addTouchLayer();
+        this.removeFromParent();
     }
+
 });
