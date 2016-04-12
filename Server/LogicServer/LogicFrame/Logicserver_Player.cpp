4d36786808a460c44f29d74758eac5fbefdfc577
yuquanac@123uServer <yuquanac@123uServer.com>
2016-04-12 11:55:33 +0800

    服务器登录组队模块代码重写，支持多个房间，多个人在同一个房间

---
+++ Server/LogicServer/LogicFrame/Logicserver_Player.cpp
@@ -0,0 +1,15 @@
+#include "Logicserver_Player.h"
+
+NS_LS_BEGIN
+
+int32_t CPlayer::SendMessageToSelf(int32_t msgID, CMessageBody* body)
+{
+    CMessage msg;
+    CMessageHead* h = new CMessageHead(uin, msgID);
+    msg.SetMessageHead(h);
+    msg.SetMessageBody(body);
+
+    return LOGICSOCKET->WriteOneMessage(msg);
+}
+
+NS_LS_END
