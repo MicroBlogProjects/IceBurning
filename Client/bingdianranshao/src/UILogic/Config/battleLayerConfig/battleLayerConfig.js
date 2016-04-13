1720f28c7f2bc06025201581fa9c73867a751caf
chenxin <1017370773@qq.com>
2016-04-14 01:02:24 +0800

    [地图]

---
+++ Client/bingdianranshao/src/UILogic/Config/battleLayerConfig/battleLayerConfig.js
@@ -0,0 +1,12 @@
+/**
+ * Created by opas on 2016/4/13.
+ */
+var battleLayerConfig = battleLayerConfig||{};
+battleLayerConfig.width = 30;
+battleLayerConfig.height =40;
+battleLayerConfig.colums=15;
+battleLayerConfig.iscanPutDown={};
+battleLayerConfig.canPutDown=[];
+battleLayerConfig.canPutDownRect=[];
+battleLayerConfig.occupied=[];
+battleLayerConfig.highlight=[];
