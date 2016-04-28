7373b75e6994e7a9784ef51f6345813e9d511815
yuquanac <yuquanac@windowspc.com>
2016-04-28 22:28:26 +0800

    修改服务器代码bug

--- Server/LogicServer/LogicFrame/Logicserver_LogicFrame.cpp
+++ Server/LogicServer/LogicFrame/Logicserver_LogicFrame.cpp
@@ -1,7 +1,7 @@
 #include "Logicserver_LogicFrame.h"
 #include "Logicserver_FrameBase.h"
 #include "LoginFrame.h"
-#include "../Network/Logicserver_Socket.h"
+#include "SyncFrame.h"
 
 NS_LS_BEGIN
 
@@ -9,6 +9,7 @@ LogicFrame* LogicFrame::instance = NULL;
 
 LogicFrame::LogicFrame()
 {
+    gettimeofday(&last_check_time_, NULL);
 }
 
 int32_t LogicFrame::HandleOneMessage(const CMessage* message)
@@ -29,12 +30,39 @@ LogicFrame* LogicFrame::Instance()
 int32_t LogicFrame::HandleMessage()
 {
     std::vector<CMessage*> msgs;
-    LOGICSOCKET->ReadMessages(msgs);
-    for (size_t i = 0; i < msgs.size(); i++)
+    int32_t ret = LOGICSOCKET->ReadMessages(msgs);
+    if (ret == success)
     {
-        LOGICFRAME->HandleOneMessage(msgs[i]);
-        delete msgs[i];
+        for (size_t i = 0; i < msgs.size(); i++)
+        {
+            LOGICFRAME->HandleOneMessage(msgs[i]);
+            delete msgs[i];
+        }
     }
+    else
+    {
+        TRACE_WARN("Connect server quit or error connect");
+    }
+    return ret;
+}
+
+int32_t LogicFrame::CheckTimer(struct timeval& now_time)
+{
+    if (!FinishOneFrame(now_time)) return fail;
+    CSyncFrame::Instance()->OnOneFrameFinished();
+    return success;
+}
+
+bool LogicFrame::FinishOneFrame(struct timeval& now_time)
+{
+    int32_t dis_ms = (now_time.tv_sec - last_check_time_.tv_sec) * 1000
+        + (now_time.tv_usec - last_check_time_.tv_usec) / 1000;
+    if (dis_ms * 6 < 100)
+    {
+        return false;
+    }
+    last_check_time_ = now_time;
+    return true;
 }
 
 
